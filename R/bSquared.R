0f78fb10ef2ae492bb9620986f69acdc2f6c5244
jcarnes1 <77592201+jcarnes1@users.noreply.github.com>
2021-04-26 20:32:40 -0400

    Add files via upload

---
+++ R/bSquared.R
@@ -0,0 +1,9 @@
+# B Squared
+#
+# This is an example function named 'bSquared' it take the side 'B' value and squares itself to make bSquared.
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+bSquared <- b * b
---
+++ R/pythTheo.R
@@ -0,0 +1,12 @@
+# Pythagorean Theorem
+#
+# This is an example function named 'pythTheo' it takes the functions aSquared, bSquared, and cSquared to get the Pythagorean Theorem.
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+pythTheo <- function() {
+  aSquared + bSquared = cSquared
+  return("Length of side a: " + aSquared + ", length of side b: " + bSquared + ", length of side c: " + cSquared)
+}
