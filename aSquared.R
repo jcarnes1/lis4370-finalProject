d7374710b8e60544c13fce42c6e8c1fbe2562c55
jcarnes1 <77592201+jcarnes1@users.noreply.github.com>
2021-04-26 20:32:56 -0400

    Delete aSquared.R

--- aSquared.R
+++
@@ -1,9 +0,0 @@
-# A Squared
-#
-# This is an example function named 'aSquared' it take the side 'A' value and squares itself to make aSquared.
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-aSquared <- a * a
