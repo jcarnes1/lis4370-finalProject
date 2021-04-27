17b4025b81cb3d8a64a8f53a28b0e316c907ecc5
jcarnes1 <77592201+jcarnes1@users.noreply.github.com>
2021-04-26 20:33:04 -0400

    Delete bSquared.R

--- bSquared.R
+++
@@ -1,9 +0,0 @@
-# B Squared
-#
-# This is an example function named 'bSquared' it take the side 'B' value and squares itself to make bSquared.
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-bSquared <- b * b
